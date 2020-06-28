a6f5c533e3274eb53cfff35bc3a04ef2be0880c1
Anshul Garg <anshul@luminescent.digital>
2020-06-28 13:53:31 +0530

    Trying to take more control over login form ! rather then using directly the OOTB one !! This will help us to enhance our learnings.

--- mystore/src/app/custom-login-form/custom-login-form.component.ts
+++ mystore/src/app/custom-login-form/custom-login-form.component.ts
@@ -9,33 +9,56 @@ import {
   AuthRedirectService,
   AuthService,
   GlobalMessageService,
+  GlobalMessageType,
 } from "@spartacus/core";
-import { LoginFormComponent } from "@spartacus/storefront";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "login",
   templateUrl: "./custom-login-form.component.html",
 })
-export class CustomLoginFormComponent extends LoginFormComponent
-  implements OnInit {
+export class CustomLoginFormComponent implements OnInit {
+  loginForm: FormGroup;
   loginAsGuest = false;
+  sub: Subscription;
 
   constructor(
-    auth: AuthService,
-    globalMessageService: GlobalMessageService,
-    fb: FormBuilder,
-    authRedirectService: AuthRedirectService
-  ) {
-    super(auth, globalMessageService, fb, authRedirectService);
-  }
+    protected auth: AuthService,
+    protected globalMessageService: GlobalMessageService,
+    protected fb: FormBuilder,
+    protected authRedirectService: AuthRedirectService
+  ) {}
 
   /*
   This step is only performed to override the exiting form valition present in LoginFormComponent
   */
   ngOnInit() {
-    this.form = new FormGroup({
+    this.loginForm = new FormGroup({
       userId: new FormControl("", Validators.minLength(4)),
       password: new FormControl("", Validators.minLength(4)),
     });
+  }
+
+  // Creating custom Login functionality to learn more about the same.
+  submitCustomLoginForm() {
+    if (this.loginForm.valid) {
+      this.customLogin();
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+  customLogin() {
+    console.log(this.loginForm);
+    const { userId, password } = this.loginForm.controls;
+    this.auth.authorize(userId.value.toLowerCase(), password.value);
+
+    if (!this.sub) {
+      this.sub = this.auth.getUserToken().subscribe((data) => {
+        if (data && data.access_token) {
+          this.globalMessageService.remove(GlobalMessageType.MSG_TYPE_ERROR);
+          this.authRedirectService.redirect();
+        }
+      });
+    }
   }
 }
